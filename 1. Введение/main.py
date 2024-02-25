364e295a16e1d245d4a923628adb4fb4c95479fe
Андрей Боев <a.boev.it@gmail.com>
2024-02-25 16:54:42 +0300

    Введение

---
+++ 1. Введение/main.py
@@ -0,0 +1,9 @@
+num1 = int(input('Введите первое число: '))
+num2 = int(input('Введите второе число: '))
+print(num1 + num2)
+
+print('text_1', 'text_2', end=' ')
+print('text_3', 'text_4', sep='\n')
+
+print(f'Сумма чисел {num1} и {num2} равна {num1 + num2}')
+print('Сумма чисел', num1, 'и', num2, 'равна', num1 + num2)
